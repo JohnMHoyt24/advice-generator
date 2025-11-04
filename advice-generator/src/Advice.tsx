2e39c67b77e400f677c38b9600df011acee6e365
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2025-11-04 20:34:54 +0000

    Initial commit

---
+++ advice-generator/src/Advice.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const Advice: React.FC = () => {
+    const [button, setButton] = useState(false);
+    const [quote, setQuote] = useState("");
+
+    return(
+        <div>
+            <p>Hello World!</p>
+        </div>
+    );
+}
+
+export default Advice;
