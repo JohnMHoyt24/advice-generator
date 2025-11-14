904cd205dfa4deff019a2ef7141fe34e45e447e1
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2025-11-14 20:05:40 +0000

    Created an async function to fetch data via an API. Applied CSS when rendering the response.

--- advice-generator/src/Advice.tsx
+++ advice-generator/src/Advice.tsx
@@ -1,17 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 
 const Advice: React.FC = () => {
-    const [button, setButton] = useState(false);
-    const [quote, setQuote] = useState("");
-    const [error, setError] = useState(null);
+    interface Advice {
+        slip: {
+            id: number;
+            advice: string;
+        }
+    }
+
+    const [advice, setAdvice] = useState<Advice | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchAdvice(){
+        setLoading(true);
+        try {
+            const response = await fetch('https://api.adviceslip.com/advice');
+
+            if(!response.ok){
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data: Advice = await response.json();
+            setAdvice(data);
+        }
+
+        catch (error) {
+            console.error("Error fetching advice: ", error);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return(
         <div>
             <div className="bg-[#4c536e] w-200 h-100 border border-hidden rounded-md p-4">
-                <p><span className="uppercase text-green-300 font-[Manrope]">Advice #</span></p>
-                <p></p>
-                <button className="bg-[#007bff] text-black p-5 border rounded-md">Click!</button>
+                <p className="text-center"><span className="uppercase text-green-300 font-[Manrope] text-[12px] font-bold">Advice #{advice?.slip.id || "?"}</span></p>
+                <p className="text-white text-center text-[25px]">"{advice?.slip.advice || "Click the button for advice"}"</p>
+                <div className="flex justify-center mt-4">
+                    <button className="bg-[#007bff] text-black p-5 border rounded-md" onClick={fetchAdvice} disabled={loading}>
+                        {loading ? "Loading..." : "Click!"}
+                    </button>
+                </div>
             </div>
         </div>
     );
