78bedc97ca2712e3d99d1823841f864eb9e46a84
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2025-11-17 20:00:09 +0000

    Fixed error with svgr()

--- advice-generator/vite.config.ts
+++ advice-generator/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import type { PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), svgr()],
+  plugins: [tailwindcss(), react(), svgr()] as unknown as PluginOption[],
 })
